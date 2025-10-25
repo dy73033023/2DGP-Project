29e9ee47d36de21d003698f465833f4fd3771aa7
dy73033023 <dy73033023@tukorea.ac.kr>
2025-10-25 12:00:00 +0900

    이미지 파일 추가

--- play_mode.py
+++ play_mode.py
@@ -7,11 +7,9 @@ import title_mode
 
 from background import Background
 from stageBlock import StageBlock
-from boy import Boy
-from ball import Ball
+from player1 import Player1
 
-
-boy = None
+player1 = None
 
 def handle_events():
     event_list = get_events()
@@ -21,13 +19,14 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.change_mode(title_mode)
         else:
-            boy.handle_event(event)
+            player1.handle_event(event)
 
 def init():
     # 스테이지 배경
     # background = Background()
     # game_world.add_object(background, 0)
 
+    # 스테이지 블록들
     # 가로로 10개 배치, 시작 x는 화면 좌측(예: 100), 간격은 32
     start_x, start_y, gap = 16, 16, 32
     stageBlocks = [StageBlock(start_x + i * gap, start_y) for i in range(25)]
@@ -39,23 +38,11 @@ def init():
     game_world.add_objects(stageBlocks3, 1)
     game_world.add_objects(stageBlocks4, 1)
 
-    global boy
+    # 플레이어 1 - 왼쪽에서 시작
+    global player1
 
-    boy = Boy()
-    game_world.add_object(boy, 1)
-
-
-    global balls
-    balls = [Ball(random.randint(100, 1500), 60, 0) for _ in range(30)]
-    game_world.add_objects(balls, 1)
-
-    # 소년과 볼 사이에 대한 충돌 검사가 필요하다는 정보를 추가
-    game_world.add_collision_pair('boy : ball', boy, None)
-
-
-    for ball in balls:
-        game_world.add_collision_pair('boy : ball', None, ball)
-
+    player1 = Player1()
+    game_world.add_object(player1, 1)  # Player1이 아니라 player1 인스턴스를 추가
 
 
 def update():
