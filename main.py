8d8ca9a27fdc11f08344fad67242005dc7825176
dy73033023 <dy73033023@tukorea.ac.kr>
2025-12-06 10:46:52 +0900

    게임 오버 모드 디버깅을 위한 임시 코드 수정

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from pico2d import open_canvas, delay, close_canvas
 import game_framework
 
-import title_mode as start_mode
+import game_over_mode as start_mode
+
 
 open_canvas(800, 600)
 game_framework.run(start_mode)
